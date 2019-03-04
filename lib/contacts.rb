9e2a89716bb6b7ffa56f4059b7b931bd510d10f2
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-04 04:54:54 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,13 +13,36 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+
 def remove_strawberry(contacts)
- contacts.each do |person, contact_details_hash|
-   contact_details_hash.each do |attribute, data|
-     if attribute == :favorite_ice_cream_flavors
-       data.delete_if{|ice_cream| ice_cream == "strawberry"} 
-     end 
-   end 
- end 
+  contacts.each do |person, data_hash|
+    data_hash.each do |k, v|
+     if k == :favorite_ice_cream_flavors
+       k.each do |v|
+         v.delete("strawberry")
+       end 
+      
+    end
+  end 
 end
+  
+# def remove_strawberry(contacts)
+#   contacts.map do |person, data_hash|
+#     binding.pry
+#     if data_hash.values.flatten.include?("strawberry")
+#       data_hash.values.flatten.delete_if{|element| element == "strawberry"}
+#       data_hash
+#     end
+#     binding.pry
+#   end 
+# end 
+
+# def remove_strawberry(contacts)
+# contacts.each do |person, contact_details_hash|
+#   contact_details_hash.each do |attribute, data|
+#     if attribute == :favorite_ice_cream_flavors
+#       data.delete_if{|ice_cream| ice_cream == "strawberry"} 
+#     end 
+#   end 
+# end 
+# end
